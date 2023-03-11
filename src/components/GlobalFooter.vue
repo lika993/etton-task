2cb44bd066cbc864ef0e96a6d7e9738a8212e605
lika993 <liliafarrahova@MacBook-Air-Lilia.local>
2023-03-11 21:15:18 +0300

    fix github url

--- src/components/GlobalFooter.vue
+++ src/components/GlobalFooter.vue
@@ -2,7 +2,7 @@
   <footer class="footer d-flex ai-ctr">
     <div class="container d-flex ai-ctr jc-spbwn">
       <span class="future-employee-name">Фаррахова Лилия Асгатовна</span>
-      <router-link to="https://github.com/lika993" class="future-employee-profile" target="_blank">https://github.com/lika993</router-link>
+      <router-link to="//github.com/lika993/etton-task" class="future-employee-profile" target="_blank">https://github.com/lika993</router-link>
     </div>
   </footer>
 </template>
